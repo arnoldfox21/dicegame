a7b1f3ee77cd962fea84967eb3ff72ca002dae4f
arnoldfox21 <arnoldfox21@gmail.com>
2017-12-19 02:40:16 +0700

    v1

---
+++ Dc.py
@@ -0,0 +1,62 @@
+import random
+
+def throw(val):
+	dice = val
+	if 6 in dice and 1 in dice:
+		winner = dice.index(6)
+		dice.remove(6)
+	elif 6 in dice:
+		winner = dice.index(6)
+		dice.remove(6)
+	elif 1 in dice:
+		winner = dice.index(1)
+		dice.remove(1)
+	else:
+		winner = 'nothing'
+		dice = dice
+	result = [dice, winner]
+	return result
+def roll(number):
+
+	NO =[1, 2, 3, 4, 5, 6]
+	res = []
+	for i in range(0, number):
+		res.append(random.choice(NO))
+
+	return res
+
+choice = input('type the number of 0 to throw dice : ')
+
+if choice == 0:
+	d = roll(4)
+	print 'result of dice : %s'%(d)
+	res = throw(d)
+	print 'dice remaining is %s with number %s'%(len(res[0]), res[0])
+
+	if len(res) == 4:
+		print 'the winner is player number %s'% (int(res[1])+1)
+		print 'game was reset'
+	else:
+		choice2 = input('type the number of 0 to throw dice again : ')	
+			
+	if choice2 == 0:
+		dc = roll(3)
+		print 'result of dice : %s'%(dc)
+		rest = throw(dc)
+		print rest[0]
+		choice2 = input('type the number of 0 to throw dice again : ')
+		if choice2 == 0:
+			d3 = roll(2)
+			print 'result of dice : %s'%(d3)
+			rest3 = throw(d3)
+			print rest3[0]
+			print 'the winner is player number %s'% (int(res[1])+1)
+		else:
+			print 'unknow command'
+	else:
+		print 'unknow command'
+      
+else:
+	print 'unknow command'
+   
+   
